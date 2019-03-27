df3fab2f8010649ca5cb11834e64d1ccdecef2ba
Geunwoo Nam <alorse45@gmail.com>
2019-03-27 13:19:27 +0900

    NIL

--- app/src/main/java/hanbit/co/kr/MainActivity.java
+++ app/src/main/java/hanbit/co/kr/MainActivity.java
@@ -6,18 +6,28 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView textView = findViewById(R.id.textView);
         textView.setText(textView.getText() +" 추가");
         textView.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
         textView.setTextSize(20);
+
         EditText editText = findViewById(R.id.editText);
         editText.setText(editText.getText() + "에디트텍스트");
         final EditText editText2 = findViewById(R.id.editText);
@@ -28,7 +38,86 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d("Button", "onClickListener called!\n");
+            }
+        });
+        button.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v){
+                Log.d("Button","onLongClick called!\n");
+                return false;
+            }
+        });
+
+        ImageView image = findViewById(R.id.imageView);
+        image.setScaleType(ImageView.ScaleType.CENTER);
+        image.setImageResource(R.drawable.ic_launcher_foreground);
+
+        final CheckBox checkBox = findViewById(R.id.checkBox);
+        final TextView textView2 = findViewById(R.id.textView);
+
+        checkBox.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                textView2.setText(""+checkBox.isChecked());
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textView2.setText("" + isChecked);
+            }
+        });
+
+        final ToggleButton toggleButton = findViewById(R.id.toggleButton);
+        final TextView textView3 = findViewById(R.id.textView);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                textView3.setText(""+isChecked);
+            }
+        });
+
+        final Switch mySwitch = findViewById(R.id.mySwitch);
+        final TextView textView4 = findViewById(R.id.textView);
+
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                textView4.setText("" + isChecked);
+            }
+        });
+
+        final RadioButton radioButton = findViewById(R.id.radioButton);
+        final TextView textView5 = findViewById(R.id.textView);
+
+        radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                textView5.setText(""+isChecked);
+            }
+        });
+
+
+
+        progressBar = findViewById(R.id.progressBar);
+
+
         Log.d("LifeCycleActivity", "onCreate()");
+    }
+    public void clickImageButton(View v) {
+
+    }
+    ProgressBar progressBar;
+    public void step(View v) {
+        progressBar.incrementProgressBy(5);
     }
 
     @Override
